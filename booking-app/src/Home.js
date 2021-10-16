e1513f128577b4d5a269dbe7476a52d31f497fb0
Justyna Gapys <justynagapys0310@gmail.com>
2021-10-16 23:06:10 +0200

    displaySelectedOffer

--- booking-app/src/Home.js
+++ booking-app/src/Home.js
@@ -1,21 +1,21 @@
 import PropTypes from 'prop-types'
-import { useState } from 'react'; //hook korzystający z dobrodziejst reacta, przechowujący stan obiektu
+import { useState, useEffect } from 'react'; //hook korzystający z dobrodziejst reacta, przechowujący stan obiektu
 import './App.css';
 
-const listBackground = { //wystylowanie 
+const listBackground = { //wystylowanie
   backgroundColor: '#FAEBD7',
   padding: 10,
   };
 
-function Offer({name, description, country, cover}) { //komponent przyjmuje tylko jeden argument (= to ten sam zapis co z props)
+function Offer({name, description, country, cover, handleClick}) { //komponent przyjmuje tylko jeden argument (= to ten sam zapis co z props)
   return (
     //dorzucenie cssów przez "offer"
-    <div className="offer" style={listBackground}>  
+    <div className="offer" style={listBackground}>
       <div className="offer__column offer__column--w150 ">
         <img src={cover} alt="" />
       </div>
       <div className="offer__column">
-        <h2 onClick={()=>console.log(name)} style={{cursor: 'pointer'}}>{name}</h2>
+        <h2 onClick={()=>handleClick(name)} style={{cursor: 'pointer'}}>{name}</h2>
         <p>{description}</p>
         <p>{country}</p>
       </div>
@@ -38,27 +38,37 @@ function Home() {
   //indeks 0 - variable -> cozy dlat
   //indeks 1 - callback
 
+   useEffect(()=>{
+      setSelectedOffer('!!!');
+   }, []);
+
   return (
     <div className="App">
       <div>
         <h1>Wybrano: {selectedOffer}</h1>
       </div>
 
-      <Offer 
-        name="Pensjonat pod świerkami" 
-        description="Zakopiańska 1, Zakopane" 
-        country="Polska" 
-        cover="https://cf.bstatic.com/xdata/images/hotel/square90/79172064.webp?k=9af8e5c75879a392d627595f8fad96d205e23ed264ec3744476b57805fa28731&o=" />
-      <Offer 
-        name="Hotel Kazimierz" 
-        //description="Zakopiańska 2, Zakopane" 
+      <Offer
+        name="Pensjonat pod świerkami"
+        description="Zakopiańska 1, Zakopane"
         country="Polska"
-        cover="https://cf.bstatic.com/xdata/images/hotel/square90/192541890.webp?k=179376106102b1cde30c1b2fd7f220f1047d3015427af40d04e0a7bb1ecd16eb&o=" />
-      <Offer 
-        name="Górski hostel" 
-        description="Zakopiańska 3, Zakopane" 
+        cover="https://cf.bstatic.com/xdata/images/hotel/square90/79172064.webp?k=9af8e5c75879a392d627595f8fad96d205e23ed264ec3744476b57805fa28731&o="
+        handleClick={setSelectedOffer}
+        />
+      <Offer
+        name="Hotel Kazimierz"
+        //description="Zakopiańska 2, Zakopane"
         country="Polska"
-        cover="https://cf.bstatic.com/xdata/images/hotel/square90/164843364.webp?k=c9b83ddbec06f9602cd3acd75506a03bc5bf250384193109fe665682f780b3ce&o=" />
+        cover="https://cf.bstatic.com/xdata/images/hotel/square90/192541890.webp?k=179376106102b1cde30c1b2fd7f220f1047d3015427af40d04e0a7bb1ecd16eb&o="
+        handleClick={setSelectedOffer}
+        />
+      <Offer
+        name="Górski hostel"
+        description="Zakopiańska 3, Zakopane"
+        country="Polska"
+        cover="https://cf.bstatic.com/xdata/images/hotel/square90/164843364.webp?k=c9b83ddbec06f9602cd3acd75506a03bc5bf250384193109fe665682f780b3ce&o="
+        handleClick={setSelectedOffer}
+        />
     </div>
   );
 }

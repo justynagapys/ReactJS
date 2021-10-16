4b168889dd341c6581ff9f0e87b8c2b3534e24f6
Justyna Gapys <justynagapys0310@gmail.com>
2021-10-16 18:46:54 +0200

    props

--- booking-app/src/App.js
+++ booking-app/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 
+function Offer({name, description, country}) { //komponent przyjmuje tylko jeden argument (= to ten sam zapis co z props)
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <p>{country}</p>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      
+      <Offer name="Pensjonat pod świerkami" description="Zakopiańska 1, Zakopane" country="Polska" />
+      <Offer name="Hotel Kazimierz" description="Zakopiańska 2, Zakopane" country="Polska" />
+      <Offer name="Górski hostel" description="Zakopiańska 3, Zakopane" country="Polska" />
     </div>
   );
 }

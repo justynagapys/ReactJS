f22a365161b7d6df5496d91053762071eec58a82
Justyna Gapys <justynagapys0310@gmail.com>
2021-11-13 18:44:59 +0100

    routingStep2

--- booking-app/src/App.js
+++ booking-app/src/App.js
@@ -4,22 +4,29 @@ import {
     Route
 } from 'react-router-dom';
 
+import NavMenu from './components/NavMenu';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
+import Page404 from './pages/Page404';
+
 import './App.css';
 
 function App() {
     return (
         <Router>
             <div className="App">
-                <nav></nav>
+                <NavMenu />
                 <main>
                     <Switch>
-                        <Route path="/contact">
+                        <Route path="/" exact> 
+                        {/* exact nie podpowiada nam wtedy innych ścieżek przez Switcha */}
+                            <Home />
+                        </Route>
+                        <Route path="/contact" exact>
                             <Contact />
                         </Route>
-                        <Route path="/">
-                            <Home />
+                        <Route path="*">
+                            <Page404 />
                         </Route>
                     </Switch>
                 </main>
---
+++ booking-app/src/components/NavMenu/index.js
@@ -0,0 +1,20 @@
+//import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+
+function NavMenu () {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/contact">Contact</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavMenu;
